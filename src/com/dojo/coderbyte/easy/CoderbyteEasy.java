dc5a6d5288a13580aed904b56aec1f5f89a83054
Andrew Smith <andrewsm@amazon.com>
2015-11-11 13:29:14 -0500

    complete reverse string problem

--- src/com/dojo/coderbyte/easy/CoderbyteEasy.java
+++ src/com/dojo/coderbyte/easy/CoderbyteEasy.java
@@ -2,4 +2,14 @@ package com.dojo.coderbyte.easy;
 
 public class CoderbyteEasy {
 
+    public static void main() {
+    }
+
+    /**
+     * Have the function FirstReverse(str) take the str parameter being passed
+     * and return the string in reversed order.
+     */
+    public String reverseString(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
 }
